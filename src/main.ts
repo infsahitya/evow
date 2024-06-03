ca1de1a912c5b76de77b7892a393d21ddfb2cf03
infsahitya <sahitya.api@gmail.com>
2024-06-03 17:26:16 +0530

    versioning enabled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { VERSION_NEUTRAL, VersioningType } from "@nestjs/common";
 import { AppModule } from "./app.module";
 import { NestFactory } from "@nestjs/core";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: VERSION_NEUTRAL,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
