622e3426bbcf26b243158cb40813a4f960f1065a
infsahitya <sahitya.api@gmail.com>
2024-06-05 09:31:35 +0530

    update

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,10 +1,27 @@
-import { Strategy } from "passport-jwt";
-import { Injectable } from "@nestjs/common";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Inject, Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { GuardTokens } from "src/constant/guard.constant";
+import authConfig from "src/config/auth.config";
+import { ConfigType } from "@nestjs/config";
 
 @Injectable()
 export default class JwtStrategy extends PassportStrategy(
   Strategy,
   GuardTokens.JWT,
-) {}
+) {
+  constructor(
+    @Inject(authConfig.KEY)
+    private readonly authConfigService: ConfigType<typeof authConfig>,
+  ) {
+    super({
+      ignoreExpiration: false,
+      secretOrKey: authConfigService.jwt.secret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  validate(payload: UserPayloadProps) {
+    return payload;
+  }
+}
